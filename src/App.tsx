bcb84262333a90030da9a9ce20121b5c997f2360
Jonas <jonathang@hotmail.com.ar>
2025-10-25 18:21:21 -0300

    feat: Enhance App component with header buttons and update sidebar styles. Introduce SCSS variables and mixins for button styling, and remove obsolete CSS file.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { SideBar } from "@components/sidebar.component";
 import { sidebarClasses } from "./constants/sidebar";
 import "@scss/sidebar.styles.scss";
 
-function App() {
+const App = () => {
   return (
     <div className={sidebarClasses.container}>
+      <div className={sidebarClasses.header}>
+        <button aria-label="minimize"></button>
+        <button aria-label="maximize"></button>
+        <button aria-label="close"></button>
+      </div>
       <SideBar />
     </div>
   );
-}
+};
 
 export default App;
--- src/constants/sidebar/index.ts
+++ src/constants/sidebar/index.ts
@@ -5,4 +5,5 @@ const BASE_CLASS_NAME = 'sidebar';
 export const sidebarClasses = {
   base: BASE_CLASS_NAME,
   container: combineBemClasses(BASE_CLASS_NAME, 'container'),
+  header: combineBemClasses(BASE_CLASS_NAME, 'header'),
 };
