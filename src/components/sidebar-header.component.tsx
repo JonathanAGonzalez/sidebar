3b20854666679bf90fee18429bb60f5b748abfde
Jonas <jonathang@hotmail.com.ar>
2025-10-25 22:58:55 -0300

    feat: Create SidebarHeader component for avatar display and refactor SideBar to utilize it. Update sidebar styles with a new background color variable.

---
+++ src/components/sidebar-header.component.tsx
@@ -0,0 +1,18 @@
+import { sidebarClasses } from "@constants/sidebar";
+import avatar from "@assets/avatar.png";
+
+export const SidebarHeader = () => {
+  return (
+    <header>
+      <div className={sidebarClasses.avatar.container}>
+        <div className={sidebarClasses.avatar.image}>
+          <img src={avatar} alt="avatar" />
+        </div>
+        <div className={sidebarClasses.avatar.information}>
+          <h3>PRODUCT DESIGNER</h3>
+          <h2>Noel Lozano Kempf</h2>
+        </div>
+      </div>
+    </header>
+  );
+};
--- src/components/sidebar.component.tsx
+++ src/components/sidebar.component.tsx
@@ -1,18 +1,5 @@
-import { sidebarClasses } from "@constants/sidebar";
-import avatar from "@assets/avatar.png";
+import { SidebarHeader } from "./sidebar-header.component";
 
 export const SideBar = () => {
-  return (
-    <header>
-      <div className={sidebarClasses.avatar.container}>
-        <div className={sidebarClasses.avatar.image}>
-          <img src={avatar} alt="avatar" />
-        </div>
-        <div className={sidebarClasses.avatar.information}>
-          <h3>PRODUCT DESIGNER</h3>
-          <h2>Noel Lozano Kempf</h2>
-        </div>
-      </div>
-    </header>
-  );
+  return <SidebarHeader />;
 };
